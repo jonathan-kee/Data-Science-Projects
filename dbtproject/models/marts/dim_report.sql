02ebf7e564f7549a36fa47a2103117ce0f67f213
jonathan kee <jonathankee99@gmail.com>
2026-07-25 21:54:50 +0800

    format code

--- dbtproject/models/marts/dim_report.sql
+++ dbtproject/models/marts/dim_report.sql
@@ -1,48 +1,60 @@
-with stg_market_depth as (
-    select * from {{ref("stg_market_depth")}}
-),
+with
+    stg_market_depth as (select * from {{ ref("stg_market_depth") }}),
 
-stg_recipe_inputs as (
-    select * from {{ref("stg_recipe_inputs")}}
-),
-stg_recipe_inputs_time as (
-    select * from {{ref("stg_recipe_inputs_time")}}
-),
-material_input_total_cost AS (select stg_recipe_inputs.original_query,
-                      stg_recipe_inputs.prefix,
-                      stg_recipe_inputs.materialInputQuantity,
-                      stg_recipe_inputs.materialInput,
-                      -- Ask Price is seller, You buy from them --
-                      stg_market_depth."AI1-AskPrice",
-                      stg_recipe_inputs.materialInputQuantity * stg_market_depth."AI1-AskPrice" as "material_input_total_cost",
-                      stg_recipe_inputs.materialOutputQuantity,
-                      stg_recipe_inputs.materialOutput
-               from stg_market_depth
-                        left join stg_recipe_inputs on stg_recipe_inputs.materialInput = stg_market_depth."Ticker"
-),
-final as (
-  select totalCost.original_query,
-       totalCost.prefix,
-totalCost.materialInputQuantity,
-totalCost.materialInput,
-totalCost."AI1-Average" as "material input's AI1-Average",
-totalCost."total cost" as "material input's total cost",
-totalCost.materialOutputQuantity,
-totalCost.materialOutput,
-stg_market_depth."AI1-Average" as "material output's AI1-Average",
-totalCost.materialOutputQuantity * stg_market_depth."AI1-Average" as "material output's total price",
-(totalCost.materialOutputQuantity * stg_market_depth."AI1-Average") - totalCost."total cost" as profit,
-(stg_recipe_inputs_time.time_ms / 60000) as "time taken per order in minutes",
-(24* 60) / (stg_recipe_inputs_time.time_ms / 60000) as "total_order_per_day"
-from stg_market_depth 
-left join totalCost on totalCost.materialOutput = stg_market_depth."Ticker"
-left join stg_recipe_inputs_time on stg_recipe_inputs_time.prefix = totalCost.prefix 
-and stg_recipe_inputs_time.materialInputQuantity = totalCost.materialInputQuantity
-and stg_recipe_inputs_time.materialInput = totalCost.materialInput
-and stg_recipe_inputs_time.materialOutputQuantity = totalCost.materialOutputQuantity
-and stg_recipe_inputs_time.materialOutput = totalCost.materialOutput
--- The problem with the above query is that I can only accurately get one of the total cost,
--- need to split the data
-)
+    stg_recipe_inputs as (select * from {{ ref("stg_recipe_inputs") }}),
+    stg_recipe_inputs_time as (select * from {{ ref("stg_recipe_inputs_time") }}),
+    material_input_total_cost as (
+        select
+            stg_recipe_inputs.original_query,
+            stg_recipe_inputs.prefix,
+            stg_recipe_inputs.materialinputquantity,
+            stg_recipe_inputs.materialinput,
+            -- Ask Price is seller, You buy from them --
+            stg_market_depth."AI1-AskPrice",
+            stg_recipe_inputs.materialinputquantity
+            * stg_market_depth."AI1-AskPrice" as "material_input_total_cost",
+            stg_recipe_inputs.materialoutputquantity,
+            stg_recipe_inputs.materialoutput
+        from stg_market_depth
+        left join
+            stg_recipe_inputs
+            on stg_recipe_inputs.materialinput = stg_market_depth."Ticker"
+    ),
+    final as (
+        select
+            totalcost.original_query,
+            totalcost.prefix,
+            totalcost.materialinputquantity,
+            totalcost.materialinput,
+            totalcost."AI1-Average" as "material input's AI1-Average",
+            totalcost."total cost" as "material input's total cost",
+            totalcost.materialoutputquantity,
+            totalcost.materialoutput,
+            stg_market_depth."AI1-Average" as "material output's AI1-Average",
+            totalcost.materialoutputquantity
+            * stg_market_depth."AI1-Average" as "material output's total price",
+            (totalcost.materialoutputquantity * stg_market_depth."AI1-Average")
+            - totalcost."total cost" as profit,
+            (
+                stg_recipe_inputs_time.time_ms / 60000
+            ) as "time taken per order in minutes",
+            (24 * 60)
+            / (stg_recipe_inputs_time.time_ms / 60000) as "total_order_per_day"
+        from stg_market_depth
+        left join totalcost on totalcost.materialoutput = stg_market_depth."Ticker"
+        left join
+            stg_recipe_inputs_time
+            on stg_recipe_inputs_time.prefix = totalcost.prefix
+            and stg_recipe_inputs_time.materialinputquantity
+            = totalcost.materialinputquantity
+            and stg_recipe_inputs_time.materialinput = totalcost.materialinput
+            and stg_recipe_inputs_time.materialoutputquantity
+            = totalcost.materialoutputquantity
+            and stg_recipe_inputs_time.materialoutput = totalcost.materialoutput
+    -- The problem with the above query is that I can only accurately get one of the
+    -- total cost,
+    -- need to split the data
+    )
 
-select * from final
+select *
+from final
