b4a255fdb107eb00c6861227297850e7f2467b56
jonathan kee <jonathankee99@gmail.com>
2026-07-25 22:07:05 +0800

    update dim_report.sql

--- dbtproject/models/marts/dim_report.sql
+++ dbtproject/models/marts/dim_report.sql
@@ -3,7 +3,7 @@ with
 
     stg_recipe_inputs as (select * from {{ ref("stg_recipe_inputs") }}),
     stg_recipe_inputs_time as (select * from {{ ref("stg_recipe_inputs_time") }}),
-    material_input_total_cost as (
+    material_input_total_cost_cte as (
         select
             stg_recipe_inputs.original_query,
             stg_recipe_inputs.prefix,
@@ -22,35 +22,38 @@ with
     ),
     material_output_total_cost as (
         select
-            material_input_total_cost.original_query,
-            material_input_total_cost.prefix,
-            material_input_total_cost.materialinputquantity,
-            material_input_total_cost.materialinput,
-            material_input_total_cost."AI1-Average" as "material input's AI1-Average",
-            material_input_total_cost."total cost" as "material input's total cost",
-            material_input_total_cost.materialoutputquantity,
-            material_input_total_cost.materialoutput,
-            stg_market_depth."AI1-Average" as "material output's AI1-Average",
-            material_input_total_cost.materialoutputquantity
-            * stg_market_depth."AI1-Average" as "material output's total price",
-            (material_input_total_cost.materialoutputquantity * stg_market_depth."AI1-Average")
-            - material_input_total_cost."total cost" as profit,
+            material_input_total_cost_cte.original_query,
+            material_input_total_cost_cte.prefix,
+            material_input_total_cost_cte.materialinputquantity,
+            material_input_total_cost_cte.materialinput,
+            material_input_total_cost_cte."AI1-AskPrice" as "material_input_buy_price",
+            material_input_total_cost_cte."material_input_total_cost",
+            material_input_total_cost_cte.materialoutputquantity,
+            material_input_total_cost_cte.materialoutput,
+            -- Bid Price is buyer, You sell to them -- 
+            stg_market_depth."AI1-BidPrice" as "material_output_sell_price",
+            material_input_total_cost_cte.materialoutputquantity
+            * stg_market_depth."AI1-BidPrice" as "material_output_total_sell_price",
             (
                 stg_recipe_inputs_time.time_ms / 60000
-            ) as "time taken per order in minutes",
+            ) as "time_taken_per_order_in_minutes",
             (24 * 60)
             / (stg_recipe_inputs_time.time_ms / 60000) as "total_order_per_day"
         from stg_market_depth
-        left join material_input_total_cost on material_input_total_cost.materialoutput = stg_market_depth."Ticker"
+        left join
+            material_input_total_cost_cte
+            on material_input_total_cost_cte.materialoutput = stg_market_depth."Ticker"
         left join
             stg_recipe_inputs_time
-            on stg_recipe_inputs_time.prefix = material_input_total_cost.prefix
+            on stg_recipe_inputs_time.prefix = material_input_total_cost_cte.prefix
             and stg_recipe_inputs_time.materialinputquantity
-            = material_input_total_cost.materialinputquantity
-            and stg_recipe_inputs_time.materialinput = material_input_total_cost.materialinput
+            = material_input_total_cost_cte.materialinputquantity
+            and stg_recipe_inputs_time.materialinput
+            = material_input_total_cost_cte.materialinput
             and stg_recipe_inputs_time.materialoutputquantity
-            = material_input_total_cost.materialoutputquantity
-            and stg_recipe_inputs_time.materialoutput = material_input_total_cost.materialoutput
+            = material_input_total_cost_cte.materialoutputquantity
+            and stg_recipe_inputs_time.materialoutput
+            = material_input_total_cost_cte.materialoutput
     -- The problem with the above query is that I can only accurately get one of the
     -- total cost,
     -- need to split the data
