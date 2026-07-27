84453d7cb6bbebba9ffd9af2a2b0feb0bbcd9555
jonathan kee <jonathankee99@gmail.com>
2026-07-27 13:18:28 +0800

    update model

--- dbtproject/models/staging/stg_cxpc_al_ai1.sql
+++ dbtproject/models/staging/stg_cxpc_al_ai1.sql
@@ -1,2 +1,5 @@
-select to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone", *
+select to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone", 
+(to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::DATE AS date_part,
+(to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::TIME AS time_part,
+*
 from {{source('prosperous_universe sources','cxpc_al_ai1_raw')}}
