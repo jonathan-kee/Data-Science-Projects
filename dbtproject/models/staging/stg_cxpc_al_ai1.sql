8fafaf4642dbc01ab0bdf48e71f0f8d240f5d066
jonathan kee <jonathankee99@gmail.com>
2026-07-27 17:02:12 +0800

    remove outlier of 3000 price

--- dbtproject/models/staging/stg_cxpc_al_ai1.sql
+++ dbtproject/models/staging/stg_cxpc_al_ai1.sql
@@ -1,5 +1,6 @@
-select to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone", 
+SELECT to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone", 
 (to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::DATE AS date_part,
 (to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::TIME AS time_part,
 *
-from {{source('prosperous_universe sources','cxpc_al_ai1_raw')}}
+FROM {{source('prosperous_universe sources','cxpc_al_ai1_raw')}}
+WHERE "High" != 3000 -- Filter out outlier
