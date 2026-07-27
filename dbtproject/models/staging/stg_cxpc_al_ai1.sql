3b1e029020eb9d0dbe2f08473cd1e6e776851dbd
jonathan kee <jonathankee99@gmail.com>
2026-07-27 17:33:14 +0800

    remove another outlier from model

--- dbtproject/models/staging/stg_cxpc_al_ai1.sql
+++ dbtproject/models/staging/stg_cxpc_al_ai1.sql
@@ -4,3 +4,4 @@ SELECT to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone",
 *
 FROM {{source('prosperous_universe sources','cxpc_al_ai1_raw')}}
 WHERE "High" != 3000 -- Filter out outlier
+and "High" != 2400 -- Filter out outlier
