04a061179bb1baf6dc3331809177a9829d7b097c
jonathan kee <jonathankee99@gmail.com>
2026-07-30 03:54:08 +0800

    update models

---
+++ dbtproject/models/staging/stg_cxpc_alo_ai1.sql
@@ -0,0 +1,22 @@
+with
+    source as (
+        select * from {{ source("prosperous_universe sources", "cxpc_alo_ai1_raw") }}
+    ),
+    renamed as (
+        select
+            to_timestamp("DateEpochMs" / 1000) at time zone 'UTC' as "timezone",
+            (to_timestamp("DateEpochMs" / 1000) at time zone 'UTC')::date as date_part,
+            (to_timestamp("DateEpochMs" / 1000) at time zone 'UTC')::time as time_part,
+            {{ adapter.quote("Interval") }},
+            {{ adapter.quote("DateEpochMs") }},
+            {{ adapter.quote("Open") }},
+            {{ adapter.quote("Close") }},
+            {{ adapter.quote("High") }},
+            {{ adapter.quote("Low") }},
+            {{ adapter.quote("Volume") }},
+            {{ adapter.quote("Traded") }}
+
+        from source
+    )
+select *
+from renamed
