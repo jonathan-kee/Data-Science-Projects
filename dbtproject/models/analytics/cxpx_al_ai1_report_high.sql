1bbb17f17dbc8317dab12ef62616a4810591f650
jonathan kee <jonathankee99@gmail.com>
2026-07-29 15:07:00 +0800

    fix model

--- dbtproject/models/analytics/cxpx_al_ai1_report_high.sql
+++ dbtproject/models/analytics/cxpx_al_ai1_report_high.sql
@@ -7,50 +7,53 @@ WITH base AS (
     FROM {{ ref("stg_cxpc_al_ai1") }}
     WHERE "Interval" = 'DAY_ONE'
 ),
-
+max_date AS (
+    SELECT MAX("date_part"::date) as max_date_part
+    from {{ ref("stg_cxpc_al_ai1") }}
+),
 begining_month_maximum_high AS (
     SELECT 
         MAX(high) AS max_high,
-        DATE_TRUNC('month', '2026-07-26'::date)::date AS start_date,
-        '2026-07-26'::date AS end_date
-    FROM base
-    WHERE date_part BETWEEN DATE_TRUNC('month', '2026-07-26'::date)::date AND '2026-07-26'::date
+        DATE_TRUNC('month', MAX(max_date.max_date_part::date))::date AS start_date,
+        MAX(max_date.max_date_part::date) AS end_date
+    FROM base inner join max_date on 1=1
+    WHERE date_part BETWEEN DATE_TRUNC('month', max_date.max_date_part::date)::date AND max_date.max_date_part::date
 ),
 
 three_month_maximum_high AS (
     SELECT 
         MAX(high) AS max_high,
-        (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '2 months')::date AS start_date,
-        '2026-07-26'::date AS end_date
-    FROM base
-    WHERE date_part BETWEEN (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '2 months')::date AND '2026-07-26'::date
+        (DATE_TRUNC('month', MAX(max_date.max_date_part::date)) - INTERVAL '2 months')::date AS start_date,
+        MAX(max_date.max_date_part::date) AS end_date
+    FROM base inner join max_date on 1=1
+    WHERE date_part BETWEEN (DATE_TRUNC('month', max_date.max_date_part::date) - INTERVAL '2 months')::date AND max_date.max_date_part::date
 ),
 
 six_month_maximum_high AS (
     SELECT 
         MAX(high) AS max_high,
-        (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '5 months')::date AS start_date,
-        '2026-07-26'::date AS end_date
-    FROM base
-    WHERE date_part BETWEEN (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '5 months')::date AND '2026-07-26'::date
+        (DATE_TRUNC('month', MAX(max_date.max_date_part::date)) - INTERVAL '5 months')::date AS start_date,
+        MAX(max_date.max_date_part::date) AS end_date
+    FROM base inner join max_date on 1=1
+    WHERE date_part BETWEEN (DATE_TRUNC('month', max_date.max_date_part::date) - INTERVAL '5 months')::date AND max_date.max_date_part::date
 ),
 
 nine_month_maximum_high AS (
     SELECT 
         MAX(high) AS max_high,
-        (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '8 months')::date AS start_date,
-        '2026-07-26'::date AS end_date
-    FROM base
-    WHERE date_part BETWEEN (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '8 months')::date AND '2026-07-26'::date
+        (DATE_TRUNC('month', MAX(max_date.max_date_part::date)) - INTERVAL '8 months')::date AS start_date,
+        MAX(max_date.max_date_part::date) AS end_date
+    FROM base inner join max_date on 1=1
+    WHERE date_part BETWEEN (DATE_TRUNC('month', max_date.max_date_part::date) - INTERVAL '8 months')::date AND max_date.max_date_part::date
 ),
 
 twelve_month_maximum_high AS (
     SELECT 
         MAX(high) AS max_high,
-        (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '11 months')::date AS start_date,
-        '2026-07-26'::date AS end_date
-    FROM base
-    WHERE date_part BETWEEN (DATE_TRUNC('month', '2026-07-26'::date) - INTERVAL '11 months')::date AND '2026-07-26'::date
+        (DATE_TRUNC('month', MAX(max_date.max_date_part::date)) - INTERVAL '11 months')::date AS start_date,
+        MAX(max_date.max_date_part::date) AS end_date
+    FROM base inner join max_date on 1=1
+    WHERE date_part BETWEEN (DATE_TRUNC('month', max_date.max_date_part::date) - INTERVAL '11 months')::date AND max_date.max_date_part::date
 ),
 
 entire_period_maximum_high AS (
@@ -59,12 +62,16 @@ entire_period_maximum_high AS (
         MIN(date_part) AS start_date,
         MAX(date_part) AS end_date
     FROM base
+),
+
+OnlyOneRow as (
+    SELECT 'today period' AS period, high AS max_high, max_date.max_date_part::date AS start_date, max_date.max_date_part::date AS end_date
+    FROM base inner join max_date on 1=1
+    WHERE date_part = max_date.max_date_part::date
 )
 
-SELECT 'today period' AS period, high AS max_high, '2026-07-26'::date AS start_date, '2026-07-26'::date AS end_date
-FROM base
-WHERE date_part = '2026-07-26'::date
-
+SELECT period AS period, max_high AS max_high, start_date AS start_date, end_date AS end_date
+FROM OnlyOneRow
 UNION ALL
 SELECT '1st month period' AS period, max_high, start_date, end_date FROM begining_month_maximum_high
 UNION ALL
