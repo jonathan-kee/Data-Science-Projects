f0d0f029e56daf28a67ef5c1076778fbe233716f
jonathan kee <jonathankee99@gmail.com>
2026-07-29 16:53:54 +0800

    update ingestion code

--- ingestion/json/cxpc_AL_AI1.js
+++ ingestion/json/cxpc_AL_AI1.js
@@ -1,5 +1,21 @@
 import { readFile, writeFile, mkdir, rename } from 'node:fs/promises';
 import { join, basename } from 'node:path';
+import axios from "axios";
+
+async function parseArray(data) {
+  try {
+    const jsonArray = JSON.parse(data);
+
+    if (!Array.isArray(jsonArray)) {
+      throw new Error('The JSON file does not contain a top-level array.');
+    }
+
+    console.log(`Loaded ${jsonArray.length} items successfully!`);
+    return jsonArray;
+  } catch (error) {
+    console.error('Failed to read or parse JSON file:', error.message);
+  }
+}
 
 async function readJsonArray(filePath) {
   try {
@@ -17,19 +33,47 @@ async function readJsonArray(filePath) {
   }
 }
 
+function getTodayDateFileName(filename){
+  const today = new Date();
+
+  const day   = String(today.getDate()).padStart(2, '0');
+  const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+  const year  = today.getFullYear();
+
+  const ddMMyyyy = `${day}${month}${year}`;
+  console.log(ddMMyyyy); // Output: "29072026
+
+  return filename + "_" + ddMMyyyy +".json";
+}
+
 async function main() {
-  const jsonFilePath = '/Users/jonathankee/Data-Science-Projects/ingestion/sources_unprocessed/cxpc_AL_AI1_29072026.json';
-  
+  const response = await axios.get("https://rest.fnar.net/exchange/cxpc/AL.AI1", {
+    headers: {
+      'accept': 'application/json'
+    },
+    timeout: 5000, // 5 seconds
+  });
+
+  console.log(typeof response.data);
+  let stringData = JSON.stringify(response.data, null, 2);
+
+  const jsonFilePath = '/Users/jonathankee/Data-Science-Projects/ingestion/sources_unprocessed/' + getTodayDateFileName('cxpc_AL_AI1');
+
+  // null 2 is to format the json properly
+  await writeFile(jsonFilePath, stringData, 'utf-8');
+  // let data = await parseArray(data);
+
+  // Read file on disk
   let data = await readJsonArray(jsonFilePath);
   if (!data) return;
 
   let lengthOfArrayOfJson = data.length;
 
   const tableName = 'raw.cxpc_AL_AI1_raw';
-  
+
   // 1. Initialize string with TRUNCATE TABLE statement
   let sb = `TRUNCATE TABLE ${tableName};\n\n`;
-  
+
   // 2. Add INSERT statement header
   sb += `INSERT INTO ${tableName} ("Interval", "DateEpochMs", "Open", "Close", "High", "Low", "Volume", "Traded")\nVALUES `;
 
