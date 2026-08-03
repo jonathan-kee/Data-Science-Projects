688b4598ef31dca94b6982cdea325cc8128ae75e
jonathan kee <jonathankee99@gmail.com>
2026-08-04 05:10:58 +0800

    update sql

--- dbtproject/models/update/intermediate_table.sql
+++ dbtproject/models/update/intermediate_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE raw.recipe_input_totals AS
-with 
+with
     joining_table as (
-        select raw.stg_recipe_inputs_time.*, 
+        select raw.stg_recipe_inputs_time.*,
         -- Ask Price is seller, You buy from them --
-        "AI1-AskPrice", 
-        -- Bid Price is buyer, You sell to them -- 
-        "AI1-BidPrice", 
+        "AI1-AskPrice",
+        -- Bid Price is buyer, You sell to them --
+        "AI1-BidPrice",
         stg_recipe_inputs_time."time_ms" / 60000.0 AS "minutes"
         from raw.stg_recipe_inputs_time
         -- Because you only join on "materialinput", you cannot count the profit of output
@@ -23,16 +23,16 @@ with
             joining_table."prefix",
             max("minutes") as "minutes per order",
             CONCAT(
-        CAST(MAX(minutes) AS INT) / 60, ' hours ', 
+        CAST(MAX(minutes) AS INT) / 60, ' hours ',
         CAST(MAX(minutes) AS INT) % 60, ' mins'
         ) AS "hour and minutes" ,
             max("minutes") / max("materialoutputquantity") as "minutes per unit",
             CONCAT(
-        CAST(max("minutes") / max("materialoutputquantity") AS INT) / 60, ' hours ', 
+        CAST(max("minutes") / max("materialoutputquantity") AS INT) / 60, ' hours ',
         CAST(max("minutes") / max("materialoutputquantity") AS INT) % 60, ' mins'
-        ) AS "hour and minutes" ,
+        ) AS "hour and minutes per unit" ,
             sum("AI1-AskPrice") / max("materialinputquantity") as "input_materials_AI1_askprice_per_unit"
-        from joining_table 
+        from joining_table
         group by
             joining_table."original_query",
             joining_table."prefix"
