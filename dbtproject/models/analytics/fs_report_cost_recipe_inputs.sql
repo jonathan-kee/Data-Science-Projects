0234fa796cf733a907449ca68ec4567374a0a791
jonathan kee <jonathankee99@gmail.com>
2026-08-04 03:01:09 +0800

    update fs_report_cost_recipe_inputs.sql

--- dbtproject/models/analytics/fs_report_cost_recipe_inputs.sql
+++ dbtproject/models/analytics/fs_report_cost_recipe_inputs.sql
@@ -10,11 +10,19 @@ with
             raw.stg_prices.date_part = (select max("date_part") from raw.stg_prices)
             and raw.stg_recipe_inputs_time."prefix" = 'FS'
     ),
+    which_material as (
+        select
+            *
+        from joining_table
+        inner join
+            raw.total_cost_recipe_inputs
+            on joining_table."materialinput"
+            = raw.total_cost_recipe_inputs."materialoutput"    )
     total_cost_recipe as (
         select
             joining_table."original_query",
-            sum(raw.total_cost_recipe_inputs."total_a1_askprice"),
-            sum(raw.total_cost_recipe_inputs."minutes per order")
+            sum(raw.total_cost_recipe_inputs."total_a1_askprice") as "total price to buy",
+            sum(raw.total_cost_recipe_inputs."minutes per order") as "minutes per order"
         from joining_table
         inner join
             raw.total_cost_recipe_inputs
