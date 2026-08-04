1a52f66ec3aa2bef369efe08966447dda7f62be5
jonathan kee <jonathankee99@gmail.com>
2026-08-04 16:24:38 +0800

    update model

--- dbtproject/models/marts/fs_recipe_report.sql
+++ dbtproject/models/marts/fs_recipe_report.sql
@@ -1,12 +1,12 @@
-select raw.recipe_table_input_aggregate.original_query,
-        raw.recipe_table_input_aggregate.prefix,
-       SPLIT_PART(raw.recipe_table_input_aggregate.original_query, 'x', -1) as "Output Material",
+select raw.fs_recipe_table_input_prices_from_report_aggregate.original_query,
+        raw.fs_recipe_table_input_prices_from_report_aggregate.prefix,
+       SPLIT_PART(raw.fs_recipe_table_input_prices_from_report_aggregate.original_query, 'x', -1) as "Output Material",
        "total input_materials_AI1_AskPrice" / "input_materials_AI1_AskPrice_per_unit" as "volume",
        "input_materials_AI1_AskPrice_per_unit",
        "total input_materials_AI1_AskPrice",
        "total output_materials_AI1_BidPrice",
        "total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice" as "profit"
 from {{ ref("fs_recipe_table_input_prices_from_report_aggregate") }} join raw.fs_recipe_table_output_aggregate
-    on raw.recipe_table_input_aggregate.original_query = raw.recipe_table_output_aggregate.original_query
-where raw.recipe_table_input_aggregate.prefix = 'SME'
+    on raw.fs_recipe_table_input_prices_from_report_aggregate.original_query = raw.fs_recipe_table_output_aggregate.original_query
+where raw.fs_recipe_table_input_prices_from_report_aggregate.prefix = 'FS'
 ORDER BY "profit" desc
--- dbtproject/models/marts/prices_from_recipe_report.sql
+++ dbtproject/models/marts/prices_from_recipe_report.sql
@@ -3,12 +3,13 @@ with
 SELECT
     raw.stg_prices.date_part,
     raw.stg_prices."Ticker",
-    raw.stg_prices."AI1-AskPrice",
+    raw.stg_prices."AI1-AskPrice" as "Original AI1-AskPrice",
+    raw.recipe_report."input_materials_AI1_AskPrice_per_unit" as "Changed AI1-AskPrice",
     CASE
         WHEN raw.recipe_report."input_materials_AI1_AskPrice_per_unit" IS NOT NULL
         THEN raw.recipe_report."input_materials_AI1_AskPrice_per_unit"
-        ELSE raw.stg_prices."AI1-BidPrice"
-    END AS "AI1-BidPrice"
+        ELSE raw.stg_prices."AI1-AskPrice"
+    END AS "AI1-AskPrice"
 FROM {{ ref("stg_prices") }}
 LEFT JOIN raw.recipe_report
     ON raw.recipe_report."Output Material" = raw.stg_prices."Ticker"
