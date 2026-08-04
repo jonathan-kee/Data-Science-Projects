f5d1ce94aecec6495867f7f94f6eccf3f5e2d0df
jonathan kee <jonathankee99@gmail.com>
2026-08-05 07:36:09 +0800

    include profit per day metric

--- dbtproject/models/marts/recipe_report.sql
+++ dbtproject/models/marts/recipe_report.sql
@@ -1,12 +1,14 @@
-select raw.recipe_table_input_aggregate.original_query,
-        raw.recipe_table_input_aggregate.prefix,
-       SPLIT_PART(raw.recipe_table_input_aggregate.original_query, 'x', -1) as "Output Material",
+select recipe_table_input_aggregate.original_query,
+       recipe_table_input_aggregate.prefix,
+       SPLIT_PART(recipe_table_input_aggregate.original_query, 'x', -1) as "Output_Material",
        "total input_materials_AI1_AskPrice" / "input_materials_AI1_AskPrice_per_unit" as "volume",
        "input_materials_AI1_AskPrice_per_unit",
        "total input_materials_AI1_AskPrice",
        "total output_materials_AI1_BidPrice",
-       "total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice" as "profit"
-from {{ ref("recipe_table_input_aggregate") }} join raw.recipe_table_output_aggregate
-    on raw.recipe_table_input_aggregate.original_query = raw.recipe_table_output_aggregate.original_query
-where raw.recipe_table_input_aggregate.prefix = 'SME'
-ORDER BY "profit" desc
+       "total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice" as "profit of buy minus sell",
+       (1440 / recipe_table_input_aggregate."minutes per order") * ("total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice") as "profit per day"
+from {{ ref("recipe_table_input_aggregate") }} as recipe_table_input_aggregate
+join {{ ref("recipe_table_output_aggregate") }} as recipe_table_output_aggregate
+    on recipe_table_input_aggregate.original_query = recipe_table_output_aggregate.original_query
+where recipe_table_input_aggregate.prefix = 'SME'
+ORDER BY "profit per day" desc
