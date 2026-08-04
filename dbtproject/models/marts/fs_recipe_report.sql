bd1ce2321f28e798dc7b31ca896f45ec374a159e
jonathan kee <jonathankee99@gmail.com>
2026-08-05 07:43:34 +0800

    update model

--- dbtproject/models/marts/fs_recipe_report.sql
+++ dbtproject/models/marts/fs_recipe_report.sql
@@ -1,12 +1,14 @@
-select raw.fs_recipe_table_input_prices_from_report_aggregate.original_query,
-        raw.fs_recipe_table_input_prices_from_report_aggregate.prefix,
-       SPLIT_PART(raw.fs_recipe_table_input_prices_from_report_aggregate.original_query, 'x', -1) as "Output Material",
+select fs_recipe_table_input_prices_from_report_aggregate.original_query,
+        fs_recipe_table_input_prices_from_report_aggregate.prefix,
+       SPLIT_PART(fs_recipe_table_input_prices_from_report_aggregate.original_query, 'x', -1) as "Output Material",
        "total input_materials_AI1_AskPrice" / "input_materials_AI1_AskPrice_per_unit" as "volume",
        "input_materials_AI1_AskPrice_per_unit",
        "total input_materials_AI1_AskPrice",
        "total output_materials_AI1_BidPrice",
-       "total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice" as "profit"
-from {{ ref("fs_recipe_table_input_prices_from_report_aggregate") }} join raw.fs_recipe_table_output_aggregate
-    on raw.fs_recipe_table_input_prices_from_report_aggregate.original_query = raw.fs_recipe_table_output_aggregate.original_query
-where raw.fs_recipe_table_input_prices_from_report_aggregate.prefix = 'FS'
-ORDER BY "profit" desc
+       "total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice" as "profit",
+       (1440 / fs_recipe_table_input_prices_from_report_aggregate."minutes per order") * ("total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice") as "profit per day"
+from {{ ref("fs_recipe_table_input_prices_from_report_aggregate") }} as fs_recipe_table_input_prices_from_report_aggregate
+join {{ ref("fs_recipe_table_output_aggregate") }} as fs_recipe_table_output_aggregate
+    on fs_recipe_table_input_prices_from_report_aggregate.original_query = fs_recipe_table_output_aggregate.original_query
+where fs_recipe_table_input_prices_from_report_aggregate.prefix = 'FS'
+ORDER BY "profit per day" desc
