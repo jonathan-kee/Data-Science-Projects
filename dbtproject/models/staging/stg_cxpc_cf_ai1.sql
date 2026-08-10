7387e2ebe12d099fc428be798efdf5ef45efd0a7
jonathan kee <jonathankee99@gmail.com>
2026-08-10 21:30:00 +0800

    update models

---
+++ dbtproject/models/staging/stg_cxpc_cf_ai1.sql
@@ -0,0 +1,9 @@
+SELECT 
+'al' as "ticker",
+to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone", 
+(to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::DATE AS date_part,
+(to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::TIME AS time_part,
+*
+FROM {{source('prosperous_universe_sources','cxpc_cf_ai1_raw')}}
+WHERE "High" != 3000 -- Filter out outlier
+and "High" != 2400 -- Filter out outlier
