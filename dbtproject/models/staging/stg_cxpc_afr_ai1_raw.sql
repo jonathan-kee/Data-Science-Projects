da6eb6b14941d1d743b14a2069abc8a12f9c807a
jonathan kee <jonathankee99@gmail.com>
2026-08-11 11:30:51 +0800

    update models

--- dbtproject/models/staging/stg_cxpc_afr_ai1_raw.sql
+++ dbtproject/models/staging/stg_cxpc_afr_ai1_raw.sql
@@ -1,9 +1,15 @@
-SELECT 
-'AL' as "ticker",
+with source as (
+        select * from {{ source('prosperous_universe_sources', 'cxpc_afr_ai1_raw') }}
+  ),
+  renamed as (
+      select
+'AFR' as "ticker",
 to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone", 
 (to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::DATE AS date_part,
 (to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::TIME AS time_part,
 *
-FROM {{source('prosperous_universe_sources','cxpc_al_ai1_raw')}}
-WHERE "High" != 3000 -- Filter out outlier
-and "High" != 2400 -- Filter out outlier
+
+      from source
+  )
+  select * from renamed
+    
--- dbtproject/models/staging/stg_cxpc_stl_ai1.sql
+++ dbtproject/models/staging/stg_cxpc_stl_ai1.sql
@@ -1,5 +1,5 @@
 SELECT 
-'stl' as "ticker",
+'STL' as "ticker",
 to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC' as "timezone", 
 (to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::DATE AS date_part,
 (to_timestamp("DateEpochMs" / 1000) AT TIME ZONE 'UTC')::TIME AS time_part,
