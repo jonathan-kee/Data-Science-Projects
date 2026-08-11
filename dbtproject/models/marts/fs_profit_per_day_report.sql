bfb99967f76ba2da46d44c9417b7cf2194a55557
jonathan kee <jonathankee99@gmail.com>
2026-08-11 21:13:22 +0800

    update model

--- dbtproject/models/marts/fs_profit_per_day_report.sql
+++ dbtproject/models/marts/fs_profit_per_day_report.sql
@@ -22,6 +22,30 @@ with
         join material_input_time on 1 = 1
         join material_output_time on 1 = 1
         where original_query = 'FS:4xAU-1xFE=>5xBGO'
+    ),
+    material_input_time_2 as (
+        select max("minutes per order") as "minutes per order"
+        from raw.recipe_table_input_aggregate
+        where original_query in ('SME:5xCUO-10xO-1xSIO=>3xCU')
+    ),
+    material_output_time_2 as (
+        select "minutes per order"
+        from raw.fs_recipe_table_output_aggregate
+        where original_query = 'FS:1xCU-300xPE=>10xBCO'
+    ),
+    profit_per_day_2 as (
+        select
+            (
+                1440 / (
+                    material_input_time."minutes per order"
+                    + material_output_time."minutes per order"
+                )
+            )
+            * "profit_per_total" as "profit_per_day"
+        from raw.fs_recipe_report
+        join material_input_time on 1 = 1
+        join material_output_time on 1 = 1
+        where original_query = 'FS:1xCU-300xPE=>10xBCO'
     )
 select *
-from profit_per_day
+from profit_per_day_2
