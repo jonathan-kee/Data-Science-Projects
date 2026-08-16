9c4bea1855a88f1b799e3f19dca3c001ee1b523a
jonathan kee <jonathankee99@gmail.com>
2026-08-16 14:02:11 +0800

    update models

---
+++ dbtproject/models/staging/stg_cxpc_workforce.sql
@@ -0,0 +1,5 @@
+SELECT
+    CAST(TO_TIMESTAMP("load time", 'DD/MM/YYYY HH24:MI:SS') AS DATE) AS date_part,
+    CAST(TO_TIMESTAMP("load time", 'DD/MM/YYYY HH24:MI:SS') AS TIME) AS time_part,
+    *
+FROM {{ source('prosperous_universe_sources', 'workforce_raw') }}
