568221dd8aebfea10e9f4072c6d242ca350e0d0c
jonathan kee <jonathankee99@gmail.com>
2026-08-18 11:35:40 +0800

    remove unneeded sql code

--- dbtproject/models/marts/fct_cxpc.sql
+++
@@ -1,8 +0,0 @@
-select *
-from {{ ref("stg_cxpc_al_ai1") }}
-union all
-select * 
-from {{ ref("stg_cxpc_alo_ai1") }}
-union all
-select *
-from {{ ref("stg_cxpc_au_ai1") }}
--- dbtproject/models/marts/fct_prices.sql
+++
@@ -1,5 +0,0 @@
-with source_data as (select * from {{ ref("stg_prices") }})
-
-select source_data.*
-from source_data
-where "ticker" is not null
