12e40fd563cc91a8033665408664557e386b0282
jonathan kee <jonathankee99@gmail.com>
2026-08-18 21:02:21 +0800

    update model

---
+++ dbtproject/models/marts/sme_cost_report.sql
@@ -0,0 +1,27 @@
+select
+    -- 1. Add a daily date identifier to stamp each snapshot
+    cast('{{ var("date_part", run_started_at.strftime("%Y-%m-%d")) }}' as date) as report_date,
+    recipe_table_input_aggregate.original_query,
+    recipe_table_input_aggregate.prefix,
+    split_part(
+        recipe_table_input_aggregate.original_query, 'x', -1
+    ) as "Output_Material",
+    "total input_materials_AI1_AskPrice"
+    / "input_materials_AI1_AskPrice_per_unit" as "volume",
+    (1440 / recipe_table_input_aggregate."minutes per order") as "order_per_day",
+    (1440 / recipe_table_input_aggregate."minutes per order") * (
+        "total output_materials_AI1_BidPrice" - "total input_materials_AI1_AskPrice"
+    ) as "profit per day",
+    (1440 / recipe_table_input_aggregate."minutes per order") * "input_materials_AI1_AskPrice_per_unit" as "cost per day",
+    sme_traded_report."traded_rank" as "traded_rank"
+from {{ ref("sme_recipe_table_input_aggregate") }} as recipe_table_input_aggregate
+join
+    {{ ref("sme_recipe_table_output_aggregate") }} as recipe_table_output_aggregate
+    on recipe_table_input_aggregate.original_query
+    = recipe_table_output_aggregate.original_query
+left join {{ ref("sme_traded_report") }} as sme_traded_report 
+    on split_part(
+        recipe_table_input_aggregate.original_query, 'x', -1
+    ) = UPPER(sme_traded_report."ticker")
+where recipe_table_input_aggregate.prefix = 'SME'
+order by "profit per day" desc
