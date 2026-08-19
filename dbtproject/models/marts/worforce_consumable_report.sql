6f8a6ed61dae458c0eba14f624d4d44a75f06b7a
jonathan kee <jonathankee99@gmail.com>
2026-08-20 00:08:00 +0800

    update model

---
+++ dbtproject/models/marts/worforce_consumable_report.sql
@@ -0,0 +1,3 @@
+select workforce.material_ticker, inventory.amount / workforce.daily_amount as "days_left"
+from {{ref("stg_workforce")}} as workforce
+join {{ref("stg_inventory")}} as inventory on material_ticker = ticker
