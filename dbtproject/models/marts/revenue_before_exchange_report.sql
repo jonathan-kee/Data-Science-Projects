cf492c863337229a2d5e30254376ac83d6664d78
jonathan kee <jonathankee99@gmail.com>
2026-08-20 21:01:43 +0800

    update model

---
+++ dbtproject/models/marts/revenue_before_exchange_report.sql
@@ -0,0 +1,30 @@
+select
+    report_date,
+    (
+        select days_left
+        from raw.days_to_support_base_report
+        where materialinput = 'RAT'
+    ) as "days_left",
+    revenue_per_day,
+    (
+        select total_selling_price
+        from raw.inventory_price_report_v2
+        where
+            report_date = (select max(report_date) from raw.inventory_price_report_v2)
+            and material_ticker = 'AL'
+    ) as "inventory_revenue",
+    (
+        revenue_per_day * (
+            select days_left
+            from raw.days_to_support_base_report
+            where materialinput = 'RAT'
+        )
+    ) + (
+        select total_selling_price
+        from raw.inventory_price_report_v2
+        where
+            report_date = (select max(report_date) from raw.inventory_price_report_v2)
+            and material_ticker = 'AL'
+    ) as "total_revenue_before_exchange"
+from raw.revenue_per_day_report
+;
