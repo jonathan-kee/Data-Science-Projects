9986e7dedb7367f2605840f0743a17b8e452e3fa
jonathan kee <jonathankee99@gmail.com>
2026-08-22 12:36:46 +0800

    update model

--- dbtproject/models/marts/budget_exchange.sql
+++ dbtproject/models/marts/budget_exchange.sql
@@ -1,45 +1,39 @@
 with
-    profit as (
+    calculation as (
         select
-            report_date,  -- ADDED: Need this here so we can join it to 'cost' later
-            "revenue_per_day"
-        from {{ ref("sme_recipe_report") }}
-        where
-            original_query = 'SME:6xALO-1xO-1xC-1xFLX=>4xAL'
-
-            -- UPDATED: Support ranges first
-            {% if var("start_date", none) is not none and var(
-                "end_date", none
-            ) is not none %}
-                and report_date
-                between '{{ var("start_date") }}' and '{{ var("end_date") }}'
-
-            -- Fallback to your old single-day logic
-            {% elif var("date_part", none) is not none %}
-                and report_date = '{{ var("date_part") }}'
-
-            -- Default when no vars are passed
-            {% else %}
-                and report_date
-                = (select max(report_date) from {{ ref("sme_recipe_report") }})
-            {% endif %}
-    )
-select
-    revenue_before_exchange_report.report_date,
-    revenue_before_exchange_report.total_revenue_before_exchange
-    / total_cost_per_day as "number_of_days_to_support_for_budget",
-    (revenue_before_exchange_report.total_revenue_before_exchange / total_cost_per_day)
-    * "revenue_per_day" as "future_revenue",
-    (
-        (
+            revenue_before_exchange_report.report_date,
             revenue_before_exchange_report.total_revenue_before_exchange
-            / total_cost_per_day
-        )
-        * "revenue_per_day"
+            / total_cost_per_day as "number_of_days_to_support_for_budget"
+        from
+            {{ ref("revenue_before_exchange_report") }}
+            as revenue_before_exchange_report
+        join
+            {{ ref("cost_per_day_report") }} as cost_per_day_report
+            on revenue_before_exchange_report.report_date
+            = cost_per_day_report.report_date
+        order by revenue_before_exchange_report.report_date
+    ),
+    future_revenue_before_exchange as (
+            {# select sme_recipe_report.report_date, number_of_days_to_support_for_budget, (revenue_per_day * 4) * number_of_days_to_support_for_budget as "total__future_revenue"
+            from {{ ref("sme_recipe_report") }} as sme_recipe_report
+            join calculation on sme_recipe_report.report_date = calculation.report_date
+            where original_query = 'SME:6xALO-1xO-1xC-1xFLX=>4xAL'
+            and sme_recipe_report.report_date = (select max(report_date)from {{ ref("sme_recipe_report") }} )  #}
+
+        select revenue_per_day_report.report_date, "total_revenue_per_day" * number_of_days_to_support_for_budget as "total_future_revenue"
+        from {{ ref("revenue_per_day_report") }} as revenue_per_day_report
+        join calculation on revenue_per_day_report.report_date = calculation.report_date
+    ),
+    future_calculation as (
+        select
+            future_revenue_before_exchange.report_date,
+            future_revenue_before_exchange.total_future_revenue
+            / total_cost_per_day as "number_of_days_to_support_for_budget"
+        from future_revenue_before_exchange as future_revenue_before_exchange
+        join
+            {{ ref("cost_per_day_report") }} as cost_per_day_report
+            on future_revenue_before_exchange.report_date = cost_per_day_report.report_date
+        order by future_revenue_before_exchange.report_date
     )
-    / total_cost_per_day as "future_number_of_days_to_support_for_budget"
-from {{ ref("revenue_before_exchange_report") }} as revenue_before_exchange_report
-join
-    {{ ref("cost_per_day_report") }} as cost_per_day_report
-    on revenue_before_exchange_report.report_date = cost_per_day_report.report_date
-order by revenue_before_exchange_report.report_date
+select *
+from future_calculation;
