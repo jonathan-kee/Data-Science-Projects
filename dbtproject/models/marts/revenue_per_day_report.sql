97d62124b774cc7b600e5c270fddd6ad70c8542b
jonathan kee <jonathankee99@gmail.com>
2026-08-22 22:56:35 +0800

    update code to use variables

--- dbtproject/models/marts/revenue_per_day_report.sql
+++ dbtproject/models/marts/revenue_per_day_report.sql
@@ -9,17 +9,17 @@
 with
     profit as (
         select 
-            report_date, -- ADDED: Need this here so we can join it to 'cost' later
+            report_date,
             "revenue_per_day"
         from {{ ref("sme_recipe_report") }}
         where
             original_query = 'SME:6xALO-1xO-1xC-1xFLX=>4xAL'
             
-            -- UPDATED: Support ranges first
+            -- Support ranges first
             {% if var("start_date", none) is not none and var("end_date", none) is not none %}
                 and report_date between '{{ var("start_date") }}' and '{{ var("end_date") }}'
             
-            -- Fallback to your old single-day logic
+            -- Fallback to your single-day logic
             {% elif var("date_part", none) is not none %}
                 and report_date = '{{ var("date_part") }}'
             
@@ -29,10 +29,9 @@ with
             {% endif %}
     )
 
--- ("profit per day" * 3) because I have 3 production of AL --
 select
     profit.report_date,
     profit."revenue_per_day",
-    4 as "production_amount",
-    profit."revenue_per_day" * 4 as "total_revenue_per_day"
+    {{ var("production_amount") }} as "production_amount",
+    profit."revenue_per_day" * {{ var("production_amount") }} as "total_revenue_per_day"
 from profit
