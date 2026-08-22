73269f29a41400043c8898306d9a46eac13e74f4
jonathan kee <jonathankee99@gmail.com>
2026-08-22 11:13:19 +0800

    update model

---
+++ dbtproject/models/marts/budget_exchange.sql
@@ -0,0 +1,9 @@
+select
+    revenue_before_exchange_report.report_date,
+    revenue_before_exchange_report.total_revenue_before_exchange
+    / total_cost_per_day as "number_of_days_to_support_for_budget"
+from {{ ref("revenue_before_exchange_report") }} as revenue_before_exchange_report
+join
+    {{ ref("cost_per_day_report") }} as cost_per_day_report
+    on revenue_before_exchange_report.report_date = cost_per_day_report.report_date
+order by revenue_before_exchange_report.report_date
