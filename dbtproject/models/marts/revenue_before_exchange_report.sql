53d6c4782dcd19cccc16d14e0c905924e0e7ddd3
jonathan kee <jonathankee99@gmail.com>
2026-08-24 15:27:57 +0800

    update code to handle null

--- dbtproject/models/marts/revenue_before_exchange_report.sql
+++ dbtproject/models/marts/revenue_before_exchange_report.sql
@@ -6,25 +6,26 @@ select
         where materialinput = 'O'
     ) as "days_left",
     revenue_per_day,
-    (
+    -- coalesce to handle when inventory has not AL -- 
+    coalesce((
         select total_selling_price
         from {{ ref("inventory_price_report_v2") }}
         where
             report_date = (select max(report_date) from {{ ref("inventory_price_report_v2") }})
             and material_ticker = 'AL'
-    ) as "inventory_revenue",
+    ), 0) as "inventory_revenue",
     (
         revenue_per_day * (
             select days_left
             from {{ ref("days_to_support_base_report") }}
             where materialinput = 'O'
         )
-    ) + (
+    ) + coalesce((
         select total_selling_price
         from {{ ref("inventory_price_report_v2") }}
         where
             report_date = (select max(report_date) from {{ ref("inventory_price_report_v2") }})
             and material_ticker = 'AL'
-    ) as "total_revenue_before_exchange"
+    ), 0) as "total_revenue_before_exchange"
 from {{ ref("revenue_per_day_report") }}
 order by report_date asc
